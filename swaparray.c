19b69cca54bef7e1cefbce6b6b694d158f6670d0
Anthony Hom <anthonyhom2@hotmail.com>
2017-09-25 00:45:21 -0400

    Added file

---
+++ swaparray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+
+srand( time(NULL) );
+
+int array[10]; // array to store 10 values
+int i = 0; // iterator for the for loop
+// insert a random value in each slot
+for (i; i < 10; i++){
+	array[i] = rand();
+}
+array[9] = 0; // last value is 0
+for (i; i < 10; i++){
+	printf("array[%d] = %d\n", i, array[i]);
+}
+
+int array2[10]; // another array to store 10 values
+int * p;
+for (i; i < 10; i++){
+	p = &array[i];
+	array2[9-i] = * p;
+}
+for (i; i < 10; i++){
+	printf("array[%d] = %d\n", i, array2[i]);
+}
+
+return 0;
+}
+
